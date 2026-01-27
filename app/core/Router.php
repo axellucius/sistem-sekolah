6f88482b9ebb5eef316fdff1da341d63040b5f9a
axellucius <luciusaxel329@gmail.com>
2026-01-27 07:42:34 +0700

    feat: setup router app

---
+++ app/core/Router.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Core;
+
+class Router
+{
+    public function run()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        if ($method == 'GET' && $uri == '/students') {
+            require_once '../app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->index();
+            return;
+        }
+
+        if ($method == 'GET' && $uri == '/students/create') {
+            require_once '../app/controllers/StudentController.php';
+            $controller = new StudentController();
+            $controller->create();
+            return;
+        }
+
+        http_response_code(404);
+        echo "Not Found Page";
+    }
+}
