6cef76de6c89c0ddd8b39225beda304e27afbda6
axellucius <luciusaxel329@gmail.com>
2026-03-31 07:55:37 +0700

    feat: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,23 +17,51 @@
     </header>
 
     <main class="grow container mx-auto">
-        <div class="mt-8">
-        <div class="bg-white shadow rounded-lg p-4">
-            <h1 class="font-bold text-2xl">Daftar Siswa</h1>
-            <p>Menampilkan daftar siswa yang terdaftar</p>
+        <div class="mt-8 space-y-4">
 
-
-            <div class="bg-white shadow rounded-lg">
+            <div class="bg-white shadow rounded-lg p-4">
+                <h1 class="font-bold text-2xl">Daftar Siswa</h1>
+                <p>Menampilkan daftar siswa yang terdaftar</p>
 
             </div>
-        </div>
+
+            <div class="bg-white shadow rounded-lg">
+                <table class="w-full">
+                    <thead class="bg-gray-200">
+                        <tr>
+                            <th class="py-2 px-4 text-left">No</th>
+                            <th class="py-2 px-4 text-left">Nama</th>
+                            <th class="py-2 px-4 text-left">NIS</th>
+                            <th class="py-2 px-4 text-left">Kelas</th>
+                            <th class="py-2 px-4 text-left">No Telepon</th>
+                            <th class="py-2 px-4">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="py-2 px-4 text-left">1</td>
+                            <td class="py-2 px-4 text-left">Andi</td>
+                            <td class="py-2 px-4 text-left">1234</td>
+                            <td class="py-2 px-4 text-left">11 TKJ 3</td>
+                            <td class="py-2 px-4 text-left">0812345678</td>
+                            <td class="py-2 px-4">
+                                <div class="flex items-center justify-center gap-4">
+                                    <a href="" class="text-green-500">Detail</a>
+                                    <a href="" class="text-yellow-500">Edit</a>
+                                    <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
     </main>
 
 
 
     <footer class="bg-gray-800 text-white">
         <div class="text-center p-4">
-            &copy <?=date('Y')?> - Sistem Sekolah SMK Kristen Immanuel
+            &copy <?= date('Y') ?> - Sistem Sekolah SMK Kristen Immanuel
         </div>
     </footer>
 </body>
